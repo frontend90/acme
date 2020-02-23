6793001a76dcff9467d2318b9d760533fe1fddee
frontend90 <leon44173@gmail.com>
2020-02-23 19:57:35 +0200

    верстка

--- js/main.js
+++ js/main.js
@@ -8,33 +8,13 @@
   });
 
   /*********    blog panel    *****************/
-
   $(function () {
-    $('.blog-button').click(function (event) {
-      event.preventDefault();
-      var get_id = this.id;
-      var get_current = $('.panel .block' + get_id);
-      $('.panel .block')
-      if (not(get_current)) {
-        removeClass('blog-open');
-      } else {
-        get_current.addClass('blog-open');
-        console.log(this.id);
-      }
+    $('.panel .btn').on('click', 'button:not(.active)', function () {
+      $(this)
+        .addClass('blog-active').siblings().removeClass('blog-active')
+        .closest('div.panel').find('div.block').removeClass('blog-active').eq($(this).index()).addClass('blog-active');
     });
   });
-
-
-  var header = document.getElementById("blog-nav");
-  var btns = header.getElementsByClassName("blog-button");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("blog-active");
-      current[0].className = current[0].className.replace(" blog-active", "");
-      this.className += " blog-active";
-    });
-  }
-
 
   /************    work slick    **************/
   $('.work-slider').slick({
