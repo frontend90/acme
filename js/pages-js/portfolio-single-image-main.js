f100c72f764dde5725a19ba4ebf7e130bb4d3ea2
frontend90 <leon44173@gmail.com>
2020-03-12 11:54:13 +0200

    верстка

---
+++ js/pages-js/portfolio-single-image-main.js
@@ -0,0 +1,25 @@
+/********************   WOW      **************/
+wow = new WOW({
+  boxClass: 'wow', // default
+  animateClass: 'animated', // default
+  offset: 0 // default
+})
+wow.init();
+
+/*********************  плавный скролл   ********************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
